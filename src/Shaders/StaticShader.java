69f234d70fb2ef57be42cd366f215dc5758fccb2
royallams <royallams777@gmail.com>
2021-09-02 23:30:50 +0200

    lwjgl libs added and Transformation ,Math Classes added

--- src/Shaders/StaticShader.java
+++ src/Shaders/StaticShader.java
@@ -1,13 +1,18 @@
 package Shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram{
 
     private static final String VERTEX_FILE = "src/Shaders/vertexShader.txt";
     private static final String FRAGMENT_FILE = "src/Shaders/fragmentShader.txt";
 
+    private int location_transformationMatrix;
     public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
      }
+
+
 
     @Override
     protected void bindAttributes() {
@@ -15,4 +20,14 @@ public class StaticShader extends ShaderProgram{
         super.bindAttribute(1, "textureCoords");
 
     }
+
+    @Override
+    protected void getAllUniformLocations() {
+        location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+    }
+
+    protected void loadTransformationMatrix(Matrix4f matrix){
+        super.loadMatrix(location_transformationMatrix,matrix);
+    }
+
 }
